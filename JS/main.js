c190e5e6de581b8200821a7019773f444975d23b
unknown <you@example.com>
2024-02-28 20:36:05 +0300

    project

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+let burgerIcon = document.querySelector(".burger-img")
+let burgerMenu = document.querySelector(".burgerMenu");
+
+burgerIcon.addEventListener("click", (e)=> {
+    burgerMenu.classList.toggle("burgerMenu-show")
+})
+
+
+
